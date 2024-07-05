376e653252b61de055611e50dcf33a16274597c3
Deep22577 <deepsingh5901@gmail.com>
2024-07-05 10:41:18 +0530

    Added day12

---
+++ Question 2.py
@@ -0,0 +1,6 @@
+# Create variables to store your name, age, and favourite hobby. Print these variables
+
+name = " deep singh"
+age = 22
+fav_hobby = "Exercising"
+print(f"Hi, My name is {name}, my age is {age} and my favourite hobby is {fav_hobby}")
